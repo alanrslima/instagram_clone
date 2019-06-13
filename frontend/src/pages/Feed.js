fcff8cc58bf93fe92de010260b5699139a75dc2a
alanrslima <alanronison.lima@gmail.com>
2019-06-13 00:39:12 -0300

    Adiciona formulario de novo post e adiciona real time

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css';
 
@@ -15,9 +16,28 @@ class Feed extends Component {
   };
 
   async componentDidMount() {
+    this.registerToSocket();
     const res = await api.get("posts");
     this.setState({ feed: res.data });
-  }
+  };
+
+  registerToSocket = () => {
+    const socket = io('http://localhost:3333');
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    socket.on('like', likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post)
+      });
+    });
+  };
+
+  handleLike = async id => {
+    api.post(`/posts/${id}/like`);
+  };
 
   render() {
     const { feed } = this.state;
@@ -35,14 +55,16 @@ class Feed extends Component {
             <img src={`http://localhost:3333/files/${post.image}`} />
             <footer>
               <div className="actions">
-                <img src={like} alt="Mais" />
+                <button type="button" onClick={() => this.handleLike(post._id)} >
+                  <img src={like} alt="Mais" />
+                </button>
                 <img src={comment} alt="Mais" />
                 <img src={send} alt="Mais" />
               </div>
               <strong>{post.likes} curtidas</strong>
               <p>
                 {post.description}
-              <span>{post.hastags}</span>
+                <span>{post.hastags}</span>
               </p>
             </footer>
           </article>
